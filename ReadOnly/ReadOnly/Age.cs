751c234807a41f55c8d608c0dd2ce79abf5c6fa7
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-01 23:35:21 +0200

    Example of using |readonly|

---
+++ ReadOnly/ReadOnly/Age.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnly
+{
+    class Age
+    {
+        readonly int year;
+        Age(int year)
+        {
+            this.year = year;
+        }
+
+        void ChangeYear()
+        {
+            //year = 1967; // Compile error if uncommented.
+        }
+    }
+}
