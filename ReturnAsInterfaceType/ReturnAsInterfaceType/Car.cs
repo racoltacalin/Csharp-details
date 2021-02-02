b4db974632b473802b0ae847c3f9d345dfe80e91
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-02 09:40:49 +0200

    Example of using |return type as interface type|

---
+++ ReturnAsInterfaceType/ReturnAsInterfaceType/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnAsInterfaceType
+{
+    public interface Car
+    {
+        // public abstract void StartEngine();
+        void StartEngine();
+    }
+
+    class Sample
+    {
+        private Car myCar;
+
+        public Sample(Car car)
+        {
+            this.myCar = car;
+        }
+
+        public Car getMyCar()
+        {
+            return this.myCar;
+        }
+
+        public void setMyCar(Car car)
+        {
+            this.myCar = car;
+        }
+
+        public void DoAnything()
+        {
+            Car localCar = null;
+            localCar = this.myCar;
+            localCar.StartEngine();
+        }
+    }
+}
---
+++ ReturnAsInterfaceType/ReturnAsInterfaceType/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReturnAsInterfaceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
