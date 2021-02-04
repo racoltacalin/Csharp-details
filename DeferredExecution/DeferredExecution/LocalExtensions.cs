bbc6e4a4799796ce2181ecf8770c7b37ecb67281
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-04 18:28:35 +0200

    Example of using |Deferred Execution|

---
+++ DeferredExecution/DeferredExecution/LocalExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredExecution
+{
+    public static class LocalExtensions
+    {
+        // Use the yield return construct in an extension method to defer execution
+        public static IEnumerable<string>
+            ConvertCollectionToUpperCase(this IEnumerable<string> source)
+        {
+            foreach (string str in source)
+            {
+                Console.WriteLine("ToUpper: source {0}", str);
+                yield return str.ToUpper();
+            }
+        }
+    }
+}
---
+++ DeferredExecution/DeferredExecution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace DeferredExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "abc", "def", "ghi", "jkl" };
+
+            var query = from str in stringArray.ConvertCollectionToUpperCase()
+                        select str;
+
+            foreach (string str in query)
+            {
+                Console.WriteLine("Main: str {0}", str);
+            }
+        }
+    }
+}
