282186ad23df65bbcd54e12453c545861c2088cb
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-04 12:16:47 +0200

    Example of using |LINQ and Iterator Methods|

---
+++ WorkWithLINQ/WorkWithLINQ/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithLINQ
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>
+            (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+
+        public static bool SequenceEquals<T>
+            (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
--- WorkWithLINQ/WorkWithLINQ/Program.cs
+++ WorkWithLINQ/WorkWithLINQ/Program.cs
@@ -6,9 +6,48 @@ namespace WorkWithLINQ
 {
     class Program
     {
-        //static void Main(string[] args)
-        //{
-        //}
+        static void Main(string[] args)
+        {
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            // It's important to keep in mind that whether you choose to write your LINQ in the query syntax
+            // used above or use method syntax instead, it's always possible to go from one form of syntax
+            // to the other. The above query written in query syntax can be written in method syntax as:
+            // var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));
+
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+
+            Console.WriteLine();
+
+            // 52 cards in a deck , so 52 / 2 =26
+            //var top = startingDeck.Take(26);
+            //var bottom = startingDeck.Skip(26);
+            //var shuffle = top.InterleaveSequenceWith(bottom);
+
+            var times = 0;
+            // We can re-use the shuffle variable from earlier
+            var shuffle = startingDeck;
+            do
+            {
+                shuffle = shuffle.Take(26).InterleaveSequenceWith(shuffle.Skip(26));
+
+                foreach (var card in shuffle)
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine();
+                times++;
+
+            } while (!startingDeck.SequenceEqual(shuffle));
+
+            Console.WriteLine(times);
+        }
 
         static IEnumerable<string> Suits()
         {
@@ -34,5 +73,8 @@ namespace WorkWithLINQ
             yield return "king";
             yield return "ace";
         }
+
+
+
     }
 }
