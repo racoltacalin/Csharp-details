a4b382ccb1f3e1d413325dd4f0957e42b9d0cdb1
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-09 21:36:15 +0200

    Example of using |record types|

---
+++ RecordTypesTestHarness/RecordTypesTestHarness/DailyTemperature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordTypesTestHarness
+{
+    public record DailyTemperature(double HighTemp, double LowTemp)
+    {
+        public double Mean => (HighTemp + LowTemp) / 2.0;
+
+        public override string ToString()
+        {
+            return $"HighTemp:{HighTemp} | LowTemp:{LowTemp} | Mean:{Mean}";
+        }
+    }
+
+
+}
--- RecordTypesTestHarness/RecordTypesTestHarness/Program.cs
+++ RecordTypesTestHarness/RecordTypesTestHarness/Program.cs
@@ -7,16 +7,49 @@ namespace RecordTypesTestHarness
     {
         static void Main(string[] args)
         {
-            var dateTimeNow = DateTime.Now;
-            var blogPost1 = new BlogPost("My First Blog Post", "Clain RVC", "This is the post - 1", dateTimeNow);
-            var blogPost2 = new BlogPost("My First Blog Post", "Clain RVC", "This is the post - 1", dateTimeNow);
-            Console.WriteLine(blogPost1 == blogPost2);
+            //var dateTimeNow = DateTime.Now;
+            //var blogPost1 = new BlogPost("My First Blog Post", "Clain RVC", "This is the post - 1", dateTimeNow);
+            //var blogPost2 = new BlogPost("My First Blog Post", "Clain RVC", "This is the post - 1", dateTimeNow);
+            //Console.WriteLine(blogPost1 == blogPost2);
 
-            // It is taking this blog post and it's destructuring it into these different variables on the left side
-            var (title, _, _, date) = blogPost1;
+            //// It is taking this blog post and it's destructuring it into these different variables on the left side
+            //var (title, _, _, date) = blogPost1;
 
-            var roundTrippedBlog = JsonSerializer.Deserialize<BlogPost>(JsonSerializer.Serialize(blogPost1));
-            Console.WriteLine(roundTrippedBlog == blogPost1);
+            //var roundTrippedBlog = JsonSerializer.Deserialize<BlogPost>(JsonSerializer.Serialize(blogPost1));
+            //Console.WriteLine(roundTrippedBlog == blogPost1);
+
+            // DailyTemperature
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+
         }
+
+        // DailyTemperature
+
+        private static DailyTemperature[] data = new DailyTemperature[]
+        {
+            new DailyTemperature(HighTemp: 57, LowTemp: 30),
+            new DailyTemperature(60, 35),
+            new DailyTemperature(63, 33),
+            new DailyTemperature(68, 29),
+            new DailyTemperature(72, 47),
+            new DailyTemperature(75, 55),
+            new DailyTemperature(77, 55),
+            new DailyTemperature(72, 58),
+            new DailyTemperature(70, 47),
+            new DailyTemperature(77, 59),
+            new DailyTemperature(85, 65),
+            new DailyTemperature(87, 65),
+            new DailyTemperature(85, 72),
+            new DailyTemperature(83, 68),
+            new DailyTemperature(77, 65),
+            new DailyTemperature(72, 58),
+            new DailyTemperature(77, 55),
+            new DailyTemperature(76, 53),
+            new DailyTemperature(80, 60),
+            new DailyTemperature(85, 66)
+        };
     }
 }
