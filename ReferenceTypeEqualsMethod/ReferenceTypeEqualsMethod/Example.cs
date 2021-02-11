b4a4734a79070ab79e140876a76d3751e0a52d08
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 21:58:33 +0200

    Example of using |Equals Method on Reference Type|

---
+++ ReferenceTypeEqualsMethod/ReferenceTypeEqualsMethod/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReferenceTypeEqualsMethod
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            Person person1a = new Person("Clain");
+            Person person1b = person1a;
+            Person person2 = new Person(person1a.ToString());
+
+            Console.WriteLine("Calling Equals:");
+            Console.WriteLine("person1a and person1b => person1a.Equals(person1b): {0}", person1a.Equals(person1b));
+            Console.WriteLine("person1a and person2  => person1a.Equals(person2): {0}", person1a.Equals(person2));
+
+            Console.WriteLine("\nCasting to an Object and calling Equals:");
+            Console.WriteLine("person1a and person1b => ((object) person1a).Equals((object) person1b): {0}", ((object)person1a).Equals((object)person1b));
+            Console.WriteLine("person1a and person2  => ((object) person1a).Equals((object) person2): {0}", ((object)person1a).Equals((object)person2));
+
+        }
+    }
+}
---
+++ ReferenceTypeEqualsMethod/ReferenceTypeEqualsMethod/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypeEqualsMethod
+{
+    // Define a reference type that does not override Equals.
+    public class Person
+    {
+        public string personName;
+
+        public Person(string name)
+        {
+            this.personName = name;
+        }
+
+        public override string ToString()
+        {
+            return this.personName;
+        }
+    }
+}
