da59958bdc6c1d538bc925fd3aed53ac5745686e
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 23:18:00 +0200

    Example of using |Notes for Inheritors|

---
+++ NotesForInheritors/NotesForInheritors/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NotesForInheritors
+{
+    class Example
+    {
+        static void Main()
+        {
+            Person p1 = new Person("Clain", "123456789");
+            Person p2 = new Person("Jack", "123456789");
+            Person p3 = new Person("Jack", "1234");
+
+            Console.WriteLine("p1.Equals(p2): {0}", p1.Equals(p2));
+            Console.WriteLine("Object.Equals(p1,p2) : {0}", Object.Equals(p1, p2));
+
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("p1.Equals(p3): {0}", p1.Equals(p3));
+            Console.WriteLine("Object.Equals(p1,p3) : {0}", Object.Equals(p1, p3));
+
+        }
+    }
+}
---
+++ NotesForInheritors/NotesForInheritors/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesForInheritors
+{
+    public class Person
+    {
+        private string idNumber;
+        private string personName;
+
+        public Person(string name, string id)
+        {
+            this.personName = name;
+            this.idNumber = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person personObj = obj as Person;
+            if (personObj == null)
+            {
+                return false;
+            }
+            else
+            {
+                return idNumber.Equals(personObj.idNumber);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.idNumber.GetHashCode();
+        }
+    }
+}
