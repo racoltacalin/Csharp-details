b28e1b632090bf771f507029d343bd560c8e469f
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 13:42:48 +0200

    Example of using |as operator|

---
+++ AsOperator/AsOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AsOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = new[] { 10, 20, 30, 40, 50 };
+            IList<int> indexable = numbers as IList<int>;
+            if (indexable != null)
+            {
+                Console.WriteLine(indexable[0] + indexable[indexable.Count - 1]);
+            }
+        }
+    }
+}
