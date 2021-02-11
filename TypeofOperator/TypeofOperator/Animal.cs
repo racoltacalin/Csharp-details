07bb9cbbb1c266386357cf612afb7a2803492447
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 14:32:10 +0200

    Example of using |typeof operator|

---
+++ TypeofOperator/TypeofOperator/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeofOperator
+{
+    public class Animal { }
+
+    public class Giraffe : Animal { }
+
+    public static class TypeOfExample
+    {
+        public static void Main()
+        {
+            object b = new Giraffe();
+            Console.WriteLine("b is Animal: {0}", b is Animal);
+            Console.WriteLine("b.GetType() == typeof(Animal): {0}", b.GetType() == typeof(Animal));
+
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("b is Giraffe: {0}", b is Giraffe);
+            Console.WriteLine("b.GetType() == typeof(Giraffe): {0}", b.GetType() == typeof(Giraffe));
+        }
+    }
+}
---
+++ TypeofOperator/TypeofOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TypeofOperator
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    void PrintType<T>() => Console.WriteLine(typeof(T));
+
+        //    Console.WriteLine(typeof(List<string>));
+        //    PrintType<int>();
+        //    PrintType<System.Int32>();
+        //    PrintType<Dictionary<int, char>>();
+
+        //    Console.WriteLine("-----------------------------------------");
+
+        //    Console.WriteLine(typeof(Dictionary<,>));
+        //}
+    }
+}
