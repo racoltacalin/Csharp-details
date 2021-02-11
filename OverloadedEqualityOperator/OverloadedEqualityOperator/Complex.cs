1458af8db0e0c0a2a7894d9bc415fd76e1c172b3
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 21:48:03 +0200

    Example of using |Overloaded Equality Operator|

---
+++ OverloadedEqualityOperator/OverloadedEqualityOperator/Complex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedEqualityOperator
+{
+    public struct Complex
+    {
+        public double re, im;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Complex && this == (Complex)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(re, im).GetHashCode();
+        }
+
+        public static bool operator ==(Complex x, Complex y)
+        {
+            return x.re == y.re && x.im == y.im;
+        }
+
+        public static bool operator !=(Complex x, Complex y)
+        {
+            return !(x == y);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0}, {1})", re, im);
+        }
+    }
+}
---
+++ OverloadedEqualityOperator/OverloadedEqualityOperator/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedEqualityOperator
+{
+    class MyClass
+    {
+        public static void Main()
+        {
+            Complex cmplx1, cmplx2;
+
+            cmplx1.re = 4.0;
+            cmplx1.im = 1.0;
+
+            cmplx2.re = 2.0;
+            cmplx2.im = 1.0;
+
+            Console.WriteLine("{0} <> {1} : {2}", cmplx1, cmplx2, cmplx1 != cmplx2);
+            Console.WriteLine("{0} <> {1} : {2}", cmplx1, cmplx2, cmplx1.Equals(cmplx2));
+
+            cmplx2.re = 4.0;
+
+            Console.WriteLine("{0} = {1}: {2}", cmplx1, cmplx2, cmplx1 == cmplx2);
+            Console.WriteLine("{0} = {1}: {2}", cmplx1, cmplx2, cmplx1.Equals(cmplx2));
+
+        }
+    }
+}
