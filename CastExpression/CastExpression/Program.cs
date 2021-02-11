630a4580ebbdcef28fd4666fbe0853ecac9d93a3
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 13:53:21 +0200

    Example of using |cast expression|

---
+++ CastExpression/CastExpression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CastExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1234.7;
+            int a = (int)x;
+            Console.WriteLine(a);
+
+            IEnumerable<int> numbers = new int[] { 10, 20, 30, 40, 50 };
+            IList<int> list = (IList<int>)numbers;
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list[1]);
+        }
+    }
+}
