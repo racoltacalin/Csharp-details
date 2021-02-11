f1f0ed74ff94a1e5651e78a685c7afdcecc3c9bb
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 22:26:19 +0200

    Example of using |Equals Method on Value Type|

---
+++ ValueTypeEqualsMethod/ValueTypeEqualsMethod/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeEqualsMethod
+{
+    public struct Person
+    {
+        private string personName;
+
+        public Person(string name)
+        {
+            this.personName = name;
+        }
+
+        public override string ToString()
+        {
+            return this.personName;
+        }
+    }
+}
---
+++ ValueTypeEqualsMethod/ValueTypeEqualsMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValueTypeEqualsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte value1 = 12;
+            int value2 = 12;
+
+            object object1 = value1;
+            object object2 = value2;
+
+            Console.WriteLine("{0} ({1}) = {2} ({3}): {4}",
+                                object1, object1.GetType().Name,
+                                object2, object2.GetType().Name,
+                                object1.Equals(object2));
+
+            Person person1 = new Person("Clain");
+            Person person2 = new Person("Clain");
+
+            Console.WriteLine("\nCalling Equals:");
+            Console.WriteLine("person1.Equals(person2): {0}", person1.Equals(person2));
+
+            Console.WriteLine("\nCasting to an Object and calling Equals:");
+            Console.WriteLine("((object)person1).Equals((object)person2): {0}", ((object)person1).Equals((object)person2));
+        }
+    }
+}
