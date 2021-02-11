810c34f0b37450e9dbbf6c4e6692a75e4343c9e9
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-11 10:09:13 +0200

    Example of using |Exception class|

---
+++ ExceptionClass/ExceptionClass/ExceptionTestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionClass
+{
+    class ExceptionTestClass
+    {
+        public static void Main()
+        {
+            int x = 0;
+            try
+            {
+                int y = 100 / x;
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine($"ArithmeticException Handler: {e}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic Exception Handler: {e}");
+            }
+        }
+    }
+    // Remarks: This class is the base class for all exceptions.When an error occurs,
+    // either the system or the currently executing application reports it by throwing
+    // an exception that contains information about the error. After an exception is
+    // thrown, it is handled by the application or by the default exception handler.
+}
---
+++ ExceptionClass/ExceptionClass/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionClass
+{
+    public class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            // This implementation contains an error in program logic:
+            // It assumes that the obj argument is not null.
+            Person p = (Person)obj;
+            return this.Name.Equals(p.Name);
+        }
+
+        public class Example
+        {
+            public static void Main()
+            {
+
+            }
+        }
+    }
+}
