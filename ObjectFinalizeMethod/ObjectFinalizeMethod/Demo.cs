e6ebbcfeb9b481b4e22ba016bfb5e6624bfc5f76
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-12 09:24:37 +0200

    Example of using |Objec.Finalize method|

---
+++ ObjectFinalizeMethod/ObjectFinalizeMethod/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectFinalizeMethod
+{
+    public class Demo
+    {
+        public static void Main()
+        {
+            ExampleClass ex = new ExampleClass();
+            ex.ShowDuration();
+        }
+    }
+}
---
+++ ObjectFinalizeMethod/ObjectFinalizeMethod/ExampleClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ObjectFinalizeMethod
+{
+    public class ExampleClass
+    {
+        Stopwatch sw;
+
+        public ExampleClass()
+        {
+            sw = Stopwatch.StartNew();
+            Console.WriteLine("Instantiated object of {0}", this);
+        }
+
+        public void ShowDuration()
+        {
+            Console.WriteLine("This istance of {0} has been in existence for {1}",
+                                this, sw.Elapsed);
+        }
+
+        ~ExampleClass()
+        {
+            Console.WriteLine("Finalizing object of {0}", this);
+            sw.Stop();
+            Console.WriteLine("This istance of {0} has been in existence for {1}",
+                                this, sw.Elapsed);
+        }
+    }
+}
