7649d7388fae7baf8a77979778eaf90b85d64620
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-12 10:28:51 +0200

    Example of using |Dispose Pattern with SafeHandles|

---
+++ DisposePatternWithSafeHandles/DisposePatternWithSafeHandles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisposePatternWithSafeHandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
