9b50f21611aeab9d32de5402a50e88b0ed92785f
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-13 13:39:22 +0200

    Example of using |Object.GetType method|

---
+++ ObjectGetTypeMethod/ObjectGetTypeMethod/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjectGetTypeMethod
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            MyBaseClass myBase = new MyBaseClass();
+            MyDerivedClass myDerived = new MyDerivedClass();
+
+            object o = myDerived;
+            MyBaseClass b = myDerived;
+
+            Console.WriteLine("myBase: Type is {0}", myBase.GetType());
+            Console.WriteLine("myDerived: Type is {0}", myDerived.GetType());
+            Console.WriteLine("object o = myDerived: Type is {0}", o.GetType());
+            Console.WriteLine("MyBaseClass b = myDerived: Type is {0}", b.GetType());
+        }
+    }
+}
