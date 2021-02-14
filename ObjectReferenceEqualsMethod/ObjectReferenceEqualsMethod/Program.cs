397c15511e761561e11b9206eac22f8095bfac32
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-14 13:19:35 +0200

    Example of using |Object.ReferenceEquals method|

---
+++ ObjectReferenceEqualsMethod/ObjectReferenceEqualsMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectReferenceEqualsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
