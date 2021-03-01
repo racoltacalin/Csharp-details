0ad6e36c3dcc1d79879fe6bf00573f56d7ca736e
racoltacalin <racolta.cvasile@hotmail.com>
2021-03-01 14:11:28 +0200

    last version for genericsDemo

--- GenericsDemo/GenericsDemo/Program.cs
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GenericsDemo.Models;
+using GenericsDemo.WithGenerics;
 using GenericsDemo.WithoutGenerics;
 using System;
 using System.Collections;
@@ -23,7 +24,10 @@ namespace GenericsDemo
 
             //ages.Add((int)23.5);
 
-           
+            Console.ReadLine();
+
+            DemonstrateTextFileStorage();
+
             Console.WriteLine();
             Console.WriteLine("Press enter to shut down...");
             Console.ReadLine();
@@ -34,27 +38,60 @@ namespace GenericsDemo
             List<Person> people = new List<Person>();
             List<LogEntry> logs = new List<LogEntry>();
 
-            string peopleFile = @"C:\Temp\people.csv";
-            string logFile = @"C:\Temp\logs.csv";
+            string peopleFile = @"C:\Users\calin.racolta\Desktop\people.csv";
+            string logFile = @"C:\Users\calin.racolta\Desktop\logs.csv";
 
             PopulateLists(people, logs);
 
-            OriginalTextFileProcessor.SavePeople(people, peopleFile);
+            /* New way of doing things - generics*/
+            GenericTextFileProcessor.SaveToTextFile<Person>(people, peopleFile);
+            GenericTextFileProcessor.SaveToTextFile<LogEntry>(logs, logFile);
 
-            var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+            var newPeople = GenericTextFileProcessor.LoadFromTextFile<Person>(peopleFile);
 
             foreach (var p in newPeople)
             {
-                Console.WriteLine($" {p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+                Console.WriteLine($" { p.FirstName } { p.IsAlive }  (IsAlive = { p.IsAlive })");
             }
 
+            Console.WriteLine();
+
+            var newLogs = GenericTextFileProcessor.LoadFromTextFile<LogEntry>(logFile);
+
+            foreach (var log in newLogs)
+            {
+                Console.WriteLine($" { log.ErrorCode } : { log.Message } at { log.TimeOfEvent.ToShortTimeString() }");
+            }
+
+
+            /* Old way of doing things - non-generics*/
+
+            //OriginalTextFileProcessor.SaveLogs(logs, logFile);
+
+            //var newLogs = OriginalTextFileProcessor.LoadLogs(logFile);
+
+            //foreach (var log in newLogs)
+            //{
+            //    Console.WriteLine($"{ log.ErrorCode }: { log.Message } at { log.TimeOfEvent.ToShortTimeString() }");
+            //}
+
+            /********
+            //OriginalTextFileProcessor.SavePeople(people, peopleFile);
+
+            //var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+
+            //foreach (var p in newPeople)
+            //{
+            //    Console.WriteLine($" {p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+            //}
+             ********/
         }
 
         private static void PopulateLists(List<Person> people, List<LogEntry> logs)
         {
             people.Add(new Person { FirstName = "Clain", LastName = "Rvc" });
             people.Add(new Person { FirstName = "Cami", LastName = "Rvc" });
-            people.Add(new Person { FirstName = "Greg", LastName = "Doucette", IsAlive = false});
+            people.Add(new Person { FirstName = "Greg", LastName = "Doucette", IsAlive = false });
 
             logs.Add(new LogEntry { Message = "I blew up", ErrorCode = 9999 });
             logs.Add(new LogEntry { Message = "I'm too awesome", ErrorCode = 1337 });
--- GenericsDemo/GenericsDemo/WithoutGenerics/OriginalTextFileProcessor.cs
+++ GenericsDemo/GenericsDemo/WithoutGenerics/OriginalTextFileProcessor.cs
@@ -11,7 +11,7 @@ namespace GenericsDemo.WithoutGenerics
         public static List<Person> LoadPeople(string filePath)
         {
             List<Person> output = new List<Person>();
-            Person p;
+            Person person;
             var lines = System.IO.File.ReadAllLines(filePath).ToList();
 
 
@@ -21,15 +21,38 @@ namespace GenericsDemo.WithoutGenerics
             foreach (var line in lines)
             {
                 var vals = line.Split(',');
-                p = new Person();
+                person = new Person();
 
-                p.FirstName = vals[0];
-                p.IsAlive = bool.Parse(vals[1]);
-                p.LastName = vals[2];
+                person.FirstName = vals[0];
+                person.IsAlive = bool.Parse(vals[1]);
+                person.LastName = vals[2];
 
-                output.Add(p);
+                output.Add(person);
             }
+            return output;
+        }
 
+        public static List<LogEntry> LoadLogs(string filePath)
+        {
+            List<LogEntry> output = new List<LogEntry>();
+            LogEntry log;
+            var lines = System.IO.File.ReadAllLines(filePath).ToList();
+
+
+            // Remove the header row
+            lines.RemoveAt(0);
+
+            foreach (var line in lines)
+            {
+                var vals = line.Split(',');
+                log = new LogEntry();
+
+                log.ErrorCode = int.Parse(vals[0]);
+                log.Message = vals[1];
+                log.TimeOfEvent = DateTime.Parse(vals[2]);
+
+                output.Add(log);
+            }
             return output;
         }
 
@@ -43,6 +66,21 @@ namespace GenericsDemo.WithoutGenerics
             foreach (var p in people)
             {
                 lines.Add($"{p.FirstName}, {p.IsAlive}, {p.LastName}");
+            }
+
+            System.IO.File.WriteAllLines(filePath, lines);
+        }
+
+        public static void SaveLogs(List<LogEntry> logs, string filePath)
+        {
+            List<string> lines = new List<string>();
+
+            // Add a header row
+            lines.Add("ErrorCode,Message,TimeOfEvent");
+
+            foreach (var log in logs)
+            {
+                lines.Add($"{log.ErrorCode}, {log.Message}, {log.TimeOfEvent}");
             }
 
             System.IO.File.WriteAllLines(filePath, lines);
