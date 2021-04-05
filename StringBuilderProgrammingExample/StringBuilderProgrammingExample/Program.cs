8a123746959234d2040f9f1906ed5ebc21a360f5
Clain <Calin.Racolta@student.utcluj.ro>
2021-04-05 10:09:49 +0300

    Example of using |StringBuilder|

---
+++ StringBuilderProgrammingExample/StringBuilderProgrammingExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderProgrammingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append(i.ToString());
+            }
+            Console.WriteLine(sb);
+
+            sb[0] = sb[9];
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
